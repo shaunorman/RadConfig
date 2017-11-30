2bb43abc7374b304f559e2d5c8d10c9418625e23
Shaun Norman <shaun@speedfreaks.com.au>
2017-11-30 22:21:26 +0800

    Basic config starting point

---
+++ RadConfig.c
@@ -0,0 +1,9 @@
+// See README.md for usage.
+
+#if !defined(CONFIG_WIFI_SSID)
+    #define CONFIG_WIFI_SSID "<SSID>"
+#endif
+
+#if !defined(CONFIG_WIFI_PASSWORD)
+    #define CONFIG_WIFI_PASSWORD "<PASSWORD>"
+#endif
